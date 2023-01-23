d8c977594c6767a9a576570c03286ea5bfbf913d
Tito Ramirez <75504060+Tito-RE@users.noreply.github.com>
2023-01-23 11:20:24 -0600

    Add core logic game

--- js/main.js
+++ js/main.js
@@ -14,3 +14,44 @@ function getComputerChoice() {
   return choice;
 }
 
+function playRound(playerSelection,computerSelection) {
+  let matchResults;
+  
+  console.log("You choose: "+playerSelection);
+  console.log("PC choose: "+computerSelection);
+
+  if (playerSelection === computerSelection) {
+    matchResults = "Tie! Nobody Wins";
+  }
+
+  if (playerSelection === "Rock" && computerSelection === "Paper") {
+    matchResults = "You Lose! Paper beats Rock";
+  } 
+
+  if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    matchResults = "You Win! Rock beats Scissors";
+  }
+
+  if (playerSelection === "Paper" && computerSelection === "Rock") {
+    matchResults = "You Win! Paper beats Rock";
+  }
+
+  if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    matchResults = "You Lose! Scissors beats Paper";
+  }
+
+  if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    matchResults = "You Lose! Rock beats Scissors";
+  }
+
+  if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    matchResults = "You Win! Scissors beats Paper";
+  }
+
+  return matchResults;
+}
+
+let playerSelection = "Rock";
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
